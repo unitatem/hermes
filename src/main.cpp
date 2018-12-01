53582dd87634f73287cf6b16f4f5bae282ea5737
Mariusz Wachowicz <wachowiczmariusz@gmail.com>
2018-12-01 11:21:50 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "my_math.h"
+
+int main() {
+    std::cout << "Works fine" << std::endl;
+    std::cout << add(5.0, 3.0) << std::endl;
+    return 0;
+}
---
+++ src/my_math.cpp
@@ -0,0 +1,9 @@
+//
+// Created by mariusz on 01.12.18.
+//
+
+#include "my_math.h"
+
+double add(double lhs, double rhs) {
+    return lhs + rhs;
+}
