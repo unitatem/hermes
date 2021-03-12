81762dad3c1329ace313aed8c0ca6ff81f736601
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2021-03-13 00:56:53 +0100

    Use benchmark word as a suffix

--- tools/test_all.sh
+++ tools/test_all.sh
@@ -5,7 +5,7 @@ set -e
 #cd ./build/tests/
 # export GTEST_COLOR=1 && ctest --extra-verbose
 
-bin/benchmark_containers/tests/benchmark_vector
+bin/containers_benchmark/tests/vector_benchmark
 
 bin/library/tests/book_test
 bin/visitor/tests/visitor_test
