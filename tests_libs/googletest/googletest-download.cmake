3936c95c6efaf33a6e138ff9f3344b0f379b2673
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2021-03-13 00:50:02 +0100

    Use generic macro to download and build googletest

--- tests_libs/googletest/googletest-download.cmake
+++ tests_libs/googletest/googletest-download.cmake
@@ -8,8 +8,8 @@ include(ExternalProject)
 ExternalProject_Add(googletest
         GIT_REPOSITORY https://github.com/google/googletest.git
         GIT_TAG master
-        SOURCE_DIR "@GOOGLETEST_ROOT@/googletest-src"
-        BINARY_DIR "@GOOGLETEST_ROOT@/googletest-build"
+        SOURCE_DIR "@DESTINATION_ROOT@/googletest-src"
+        BINARY_DIR "@DESTINATION_ROOT@/googletest-build"
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
