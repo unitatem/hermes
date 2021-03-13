296fa00c3a2490f29787b3ee3d6dfc9058a938a8
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2021-03-13 01:14:28 +0100

    Build benchmark and googletest in Release

--- tests/containers_benchmark/vector.cpp
+++ tests/containers_benchmark/vector.cpp
@@ -1,1 +1,9 @@
 #include <iostream>
+
+#include <benchmark/benchmark.h>
+
+static void BM_StringCreation(benchmark::State& state) {
+  for (auto _ : state)
+    std::string empty_string;
+}
+BENCHMARK(BM_StringCreation);
