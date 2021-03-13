53840fef3107d7df9808f5a351783e786bfc10ef
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2021-03-13 10:25:48 +0100

    Build benchmark in Release type

--- tests/containers_benchmark/vector.cpp
+++ tests/containers_benchmark/vector.cpp
@@ -1,1 +1,8 @@
-#include <iostream>
+#include "benchmark/benchmark.h"
+
+static void BM_StringCopy(benchmark::State& state) {
+  std::string x = "hello";
+  for (auto _ : state)
+    std::string copy(x);
+}
+BENCHMARK(BM_StringCopy);
