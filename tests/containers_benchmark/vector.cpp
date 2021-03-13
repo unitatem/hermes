47cebbd8f79bee031f18d91bc706d6f512293934
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2021-03-13 01:35:44 +0100

    Revert "Build benchmark and googletest in Release"
    This reverts commit 296fa00c3a2490f29787b3ee3d6dfc9058a938a8.

--- tests/containers_benchmark/vector.cpp
+++ tests/containers_benchmark/vector.cpp
@@ -1,9 +1,1 @@
 #include <iostream>
-
-#include <benchmark/benchmark.h>
-
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
-}
-BENCHMARK(BM_StringCreation);
