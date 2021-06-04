c138a0b6bcac8b88bfe8a9c8532838c7129ae0c8
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2021-06-04 20:19:11 +0200

    Use ctest instead of running tests by hand

--- tools/test_all.sh
+++ tools/test_all.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-#cd ./build/tests/
-# export GTEST_COLOR=1 && ctest --extra-verbose
-
 bin/containers_benchmark/tests/vector_benchmark
 
-bin/library/tests/book_test
-bin/library/tests/matcher_test
-bin/visitor/tests/visitor_test
+cd ./build/tests/
+export GTEST_COLOR=1 && ctest --extra-verbose
