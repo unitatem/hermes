758940d9d9d0f071696b63ae1d9e037f8dcad204
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2021-08-08 18:49:32 +0200

    Make MessageQueue thread safe

--- src/message_queue/message_queue.hpp
+++ src/message_queue/message_queue.hpp
@@ -1,25 +1,42 @@
 #ifndef MESSAGE_QUEUE_HPP
 #define MESSAGE_QUEUE_HPP
 
+#include <optional>
 #include <queue>
 #include <stdexcept>
 
-// It is not thread safe.
+#include "semaphore.hpp"
+
 template <typename T>
 class MessageQueue {
  public:
-  void send(T msg) { queue.push(msg); }
+  MessageQueue() : queue_semaphore(0) {}
 
-  T receive() {
-    if (queue.empty()) throw std::out_of_range("MessageQueue is empty.");
+  void send(T msg) {
+    std::scoped_lock<decltype(queue_mutex)> lock(queue_mutex);
+    queue.push(msg);
 
+    queue_semaphore.release();
+  }
+
+  // Active listener
+  std::optional<T> receive() {
+    std::chrono::milliseconds duration(100);
+    if (!queue_semaphore.try_acquire_for(duration)) {
+      return {};
+    }
+
+    std::scoped_lock<decltype(queue_mutex)> lock(queue_mutex);
     auto e = queue.front();
     queue.pop();
+
     return e;
   }
 
  private:
   std::queue<T> queue;
+  std::mutex queue_mutex;
+  Semaphore queue_semaphore;
 };
 
 #endif  // MESSAGE_QUEUE_HPP
--- tests/message_queue/message_queue_test.cpp
+++ tests/message_queue/message_queue_test.cpp
@@ -1,27 +1,98 @@
 #include "../../src/message_queue/message_queue.hpp"
 
+#include <algorithm>
 #include <iostream>
+#include <thread>
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
-TEST(MessageQueueTest, send_receive) {
+template <typename T>
+void check_optional(const T& ref, const std::optional<T>& opt) {
+  EXPECT_TRUE(opt);
+  EXPECT_EQ(*opt, ref);
+}
+
+TEST(MessageQueueTest_SingleThread, send_receive) {
   MessageQueue<int> mq;
 
   // SUT
   mq.send(1);
   mq.send(2);
 
-  EXPECT_EQ(1, mq.receive());
-  EXPECT_EQ(2, mq.receive());
+  check_optional(1, mq.receive());
+  check_optional(2, mq.receive());
 }
 
-TEST(MessageQueueTest, send_receive_from_empty_queue) {
+TEST(MessageQueueTest_SingleThread, send_receive_from_empty_queue) {
   MessageQueue<int> mq;
 
   // SUT
   mq.send(1);
 
-  EXPECT_EQ(1, mq.receive());
-  EXPECT_THROW(mq.receive(), std::out_of_range);
+  check_optional(1, mq.receive());
+  EXPECT_FALSE(mq.receive());
+}
+
+TEST(MessageQueueTest_MultiThread, send_receive) {
+  MessageQueue<int> mq;
+
+  // SUT
+  std::thread t_r{[&]() {
+    for (int i = 1; i <= 2;) {
+      auto e = mq.receive();
+      if (!e) continue;
+
+      check_optional(i, e);
+      ++i;
+    }
+  }};
+
+  std::thread t_s{[&]() {
+    for (int i = 1; i <= 2; ++i) {
+      mq.send(i);
+    }
+  }};
+
+  t_s.join();
+  t_r.join();
+}
+
+TEST(MessageQueueTest_MultiThread, send_receive_2) {
+  constexpr int producers_cnt = 10;
+  constexpr int work_per_producer_cnt = 5000;
+  constexpr int total_work_cnt = producers_cnt * work_per_producer_cnt;
+
+  MessageQueue<int> mq;
+  // SUT
+  std::thread t_r{[&]() {
+    std::vector<int> cnt_array(work_per_producer_cnt, 0);
+    for (int i = 0; i < total_work_cnt;) {
+      auto e = mq.receive();
+      if (!e) continue;
+
+      ++cnt_array[*e];
+      ++i;
+    }
+    auto it = std::find_if(cnt_array.begin(), cnt_array.end(),
+                           [](const int e) { return e != producers_cnt; });
+    EXPECT_EQ(it, cnt_array.end()) << "Unexpected amount of work done: " << *it;
+  }};
+
+  auto worker = [&]() {
+    for (int i = 0; i < work_per_producer_cnt; ++i) {
+      mq.send(i);
+    }
+  };
+
+  std::vector<std::thread> threads;
+  for (int i = 0; i < producers_cnt; ++i) {
+    // std::thread t_s1{worker};
+    threads.emplace_back(worker);
+  }
+
+  for (auto& t : threads) {
+    t.join();
+  }
+  t_r.join();
 }
