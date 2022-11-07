3b8c7b9c18744655d176d0e833acc59840707eab
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2022-11-07 20:43:08 +0100

    Define Person datamodel with protobuf

---
+++ src/protobuf/tests/codec_test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "../person.pb.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+TEST(CodecTest, encode_decode) {
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  Person in;
+  in.set_name("Name");
+  in.set_surname("Surname");
+  std::cout << in.DebugString();
+
+  std::string wire;
+  in.SerializeToString(&wire);
+
+  Person out;
+  out.ParseFromString(wire);
+
+  EXPECT_EQ(in.name(), out.name());
+  EXPECT_EQ(in.surname(), out.surname());
+}
