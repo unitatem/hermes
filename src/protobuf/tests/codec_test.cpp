d115e30d3bd8e0b7b75cc1536bbb9ecd05f74341
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2022-11-07 22:25:56 +0100

    Generate C++ files from protobuf datamodel during compilation time

--- src/protobuf/tests/codec_test.cpp
+++ src/protobuf/tests/codec_test.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-#include "../person.pb.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "src/protobuf/person.pb.h"
 
 TEST(CodecTest, encode_decode) {
   GOOGLE_PROTOBUF_VERIFY_VERSION;
