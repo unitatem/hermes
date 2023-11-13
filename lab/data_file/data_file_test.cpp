8c7da88b8de239571d756772e739e03715c9eb1b
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2023-11-13 23:32:03 +0100

    Add a data file as a test dependency

---
+++ lab/data_file/data_file_test.cpp
@@ -0,0 +1,22 @@
+#include <filesystem>
+#include <fstream>
+#include <string>
+
+#include "gtest/gtest.h"
+
+namespace fs = std::filesystem;
+
+TEST(DataFileTest, loadFile) {
+  const std::string myPath{"lab/data_file/resources/config.txt"};
+  // Read-only mode is necessary when running under bazel sandbox
+  std::ifstream myFile{myPath};
+
+  if (!myFile.is_open()) {
+    std::cerr << "Error: " << strerror(errno) << std::endl;
+  }
+  EXPECT_TRUE(myFile.is_open());
+
+  std::string myPayload;
+  myFile >> myPayload;
+  EXPECT_EQ("secret", myPayload);
+}
